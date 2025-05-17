003e036ff02522b28ef325875a75b83aef167e50
Ifra <ifraaabdulrauf@gmail.com>
2025-05-17 12:56:12 +0500

    script for activations funcs to be used later

---
+++ utils/activations.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+def sigmoid_derivative(z):
+    s = sigmoid(z)
+    return s * (1 - s)
+
+def relu(z):
+    return np.maximum(0, z)
+
+def relu_derivative(z):
+    return (z > 0).astype(float)
